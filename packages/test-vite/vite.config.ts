faf911dd0592529bea16cdefb864da0c88aed338
Guillaume Hermet <guillaume.hermet@zama.ai>
2026-03-01 13:40:03 +0100

    refactor(test-vite): use regex for chunk matching

--- packages/test-vite/vite.config.ts
+++ packages/test-vite/vite.config.ts
@@ -4,4 +4,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (/node_modules\/(react|react-dom|react-router)\//.test(id)) {
+            return "react";
+          }
+          if (/node_modules\/(viem|wagmi)\//.test(id)) {
+            return "web3";
+          }
+        },
+      },
+    },
+  },
 });
