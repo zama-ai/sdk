88e1bda8226247f8032d71249723d252aef69c50
enitrat <msaug@protonmail.com>
2026-03-03 13:11:25 +0100

    chore: move mock contracts

--- packages/playwright/fixtures/cleartext-mock/bytecode.ts
+++ packages/playwright/fixtures/cleartext-mock/bytecode.ts
@@ -1,3 +1,3 @@
-import artifact from "../../contracts/out/CleartextFHEVMExecutor.sol/CleartextFHEVMExecutor.json";
+import artifact from "../../mock-contracts/out/CleartextFHEVMExecutor.sol/CleartextFHEVMExecutor.json";
 
 export const CLEARTEXT_EXECUTOR_BYTECODE: string = artifact.deployedBytecode.object;
