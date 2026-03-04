877e3e7465a7e102f28b91338338a9ac7d088797
enitrat <msaug@protonmail.com>
2026-03-04 13:24:25 +0100

    refactor: move hardhat-specific config out of source code

--- packages/sdk/src/relayer/cleartext/presets.ts
+++ packages/sdk/src/relayer/cleartext/presets.ts
@@ -1,20 +1,4 @@
-import deployments from "../../../../../hardhat/deployments.json" with { type: "json" };
-import { GATEWAY_CHAIN_ID, VERIFYING_CONTRACTS } from "./constants";
 import type { CleartextChainConfig } from "./types";
-
-export const hardhat = {
-  chainId: 31_337n,
-  gatewayChainId: GATEWAY_CHAIN_ID,
-  rpcUrl: "http://127.0.0.1:8545",
-  contracts: {
-    acl: deployments.fhevm.acl,
-    executor: deployments.fhevm.executor,
-    inputVerifier: deployments.fhevm.inputVerifier,
-    kmsVerifier: deployments.fhevm.kmsVerifier,
-    verifyingInputVerifier: VERIFYING_CONTRACTS.inputVerification,
-    verifyingDecryption: VERIFYING_CONTRACTS.decryption,
-  },
-} satisfies CleartextChainConfig;
 
 export const hoodi = {
   chainId: 560048n,
