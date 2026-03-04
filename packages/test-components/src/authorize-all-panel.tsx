ac739f9788d9ed4c56cc941cfabd16f3322413d7
enitrat <msaug@protonmail.com>
2026-03-04 16:11:43 +0100

    update authorize-all test

--- packages/test-components/src/authorize-all-panel.tsx
+++ packages/test-components/src/authorize-all-panel.tsx
@@ -8,7 +8,7 @@ export function AuthorizeAllPanel({ tokenAddresses }: { tokenAddresses: Address[
   return (
     <section className="space-y-2">
       <button
-        onClick={() => authorizeAll.mutate(tokenAddresses)}
+        onClick={() => authorizeAll.mutate({ tokenAddresses })}
         disabled={authorizeAll.isPending}
         className="px-4 py-2 bg-zama-yellow text-zama-black font-medium rounded hover:bg-zama-yellow-hover disabled:opacity-50 transition-colors"
         data-testid="authorize-all-button"
